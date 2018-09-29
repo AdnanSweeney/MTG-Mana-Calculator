5382c2983f4528b606927abd05c03c308a70a0a2
adnan <adnansweeney@gmail.com>
2018-09-29 19:17:56 -0400

    Added basic intiial state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      
+      totalLandCount: 17,
+
+      blackSymbols: 0,
+      blueSymbols: 0,
+      greenSymbols: 0,
+      redSymbols: 0,
+      whiteSymbols: 0,
+      greySymbols: 0
+
+      }
+  }
   render() {
+
     return (
+
       <div className="App">
+
         <header className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
+
           <h1 className="App-title">Welcome to React</h1>
+
         </header>
+
         <p className="App-intro">
+
           To get started, edit <code>src/App.js</code> and save to reload.
+
         </p>
+
       </div>
     );
   }
