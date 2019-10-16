17ec015a507a5a22d6e2c6b1bc6a000e1a79ff1b
Adnan <adnansweeney@gmail.com>
2019-10-15 23:26:21 -0400

    Trim trailing zeros on decimal values

--- src/helpers/calculateLandsFromSymbols.js
+++ src/helpers/calculateLandsFromSymbols.js
@@ -1,19 +1,22 @@
 export function calculateLandsFromSymbols(numLands, blueSymbols, redSymbols, greenSymbols, blackSymbols, whiteSymbols, greySymbols) {
 
-  let numLandsJson = { blueLands: 0, redLands: 0, greenLands: 0, blackLands: 0, whiteLands: 0, greyLands: 0 }
+	let totalSymbols = blueSymbols + redSymbols + greenSymbols + blackSymbols + whiteSymbols + greySymbols;
 
-  let totalSymbols = blueSymbols + redSymbols + greenSymbols + blackSymbols + whiteSymbols + greySymbols;
 
-  if (totalSymbols !== 0) {
+	// Initialize return object containing suggested land counts
+	let numLandsJson = { blueLands: 0, redLands: 0, greenLands: 0, blackLands: 0, whiteLands: 0, greyLands: 0 }
 
-  	numLandsJson.blueLands  = (blueSymbols  * numLands / totalSymbols).toFixed(2);
-	numLandsJson.redLands   = (redSymbols   * numLands / totalSymbols).toFixed(2);
-	numLandsJson.greenLands = (greenSymbols * numLands / totalSymbols).toFixed(2);
-	numLandsJson.blackLands = (blackSymbols * numLands / totalSymbols).toFixed(2);
-	numLandsJson.whiteLands = (whiteSymbols * numLands / totalSymbols).toFixed(2);
-	numLandsJson.greyLands  = (greySymbols  * numLands / totalSymbols).toFixed(2);
+	if (totalSymbols !== 0) {
 
-  }
-  
-  return numLandsJson;
+		// Populate land count object by looking at total land count to symbol count ratios
+		numLandsJson.blueLands  = parseFloat((blueSymbols  * numLands / totalSymbols).toFixed(2));
+		numLandsJson.redLands   = parseFloat((redSymbols   * numLands / totalSymbols).toFixed(2));
+		numLandsJson.greenLands = parseFloat((greenSymbols * numLands / totalSymbols).toFixed(2));
+		numLandsJson.blackLands = parseFloat((blackSymbols * numLands / totalSymbols).toFixed(2));
+		numLandsJson.whiteLands = parseFloat((whiteSymbols * numLands / totalSymbols).toFixed(2));
+		numLandsJson.greyLands  = parseFloat((greySymbols  * numLands / totalSymbols).toFixed(2));
+
+	}
+
+	return numLandsJson;
 }
